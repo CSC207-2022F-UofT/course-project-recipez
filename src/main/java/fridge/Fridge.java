add66f78a065f18d48b901b1aacb2fec34c508f4
dangphuong <samira.dang@mail.utoronto.ca>
2022-11-06 17:09:19 -0500

    Added docstring

--- src/main/java/fridge/Fridge.java
+++ src/main/java/fridge/Fridge.java
@@ -3,22 +3,32 @@ package fridge;
 import java.util.ArrayList;
 
 public class Fridge {
-    public ArrayList<Ingredient> ingredients;
+    /**
+     * Class Fridge:
+     * ArrayList<Ingredient> ingredients: list of available ingredients that user added.
+     * User can make changes to the ingredients list by adding or removing an ingredient.
+     * Existing user can set their Ingredient to their previous ingredients list.
+     */
+    private ArrayList<Ingredient> ingredients;
 
     public void setIngredients(ArrayList<Ingredient> ingredients) {
+//      Set Ingredient to a list of existing ingredients
         this.ingredients = ingredients;
     }
 
     public void addIngredient (Ingredient item) {
+//      Add an ingredient to Ingredient
         ingredients.add(item);
     }
 
     public void removeIngredient (Ingredient item) {
+//      Remove an ingredient from Ingredient
         ingredients.remove(item);
     }
 
     @Override
     public String toString() {
+//      Print the list of ingredients in Ingredient
         return super.toString();
     }
 }
--- src/main/java/fridge/Ingredient.java
+++ src/main/java/fridge/Ingredient.java
@@ -1,9 +1,14 @@
 package fridge;
 
 public class Ingredient {
+    /**
+     * Class ingredient:
+     * String name: name of the ingredient
+     */
     private String name;
 
     public String getName() {
+//      print the name of the ingredient
         return name;
     }
 }
