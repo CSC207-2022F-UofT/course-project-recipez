c6baf62c34dddfc392182bdd0439b2e3fdef86be
RedFoxity <victorj.li@mail.utoronto.ca>
2022-11-09 17:05:06 -0500

    Created recipe object with constructor currently accepting name, list of ingredients, time to prepare, price of recipe, and description.
    Created recipeBuilder for API caller to interact with to generate instances of recipe.

---
+++ src/main/java/useCases/recipeBuilder.java
@@ -0,0 +1,18 @@
+package useCases;
+
+import entities.recipe;
+import entities.ingredient;
+
+import java.util.ArrayList;
+
+public class recipeBuilder {
+    /**
+     *recipeBuilder class, not to be instantiated.
+     *Handles the creation of recipes using API output.
+     *createRecipe takes input from collected API information and uses it to create a new recipe instance.
+     */
+    public static recipe createRecipe(String name,ArrayList<ingredient> ingList, int time, int price, String info){
+        recipe n = new recipe(name, ingList, time, price, info);
+        return n;
+    }
+}
