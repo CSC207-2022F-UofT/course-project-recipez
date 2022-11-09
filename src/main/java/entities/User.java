05845aa119e113d4a5cef6977d79187f05975862
Shaffaan Aamir <shaffaanbin.aamir@mail.utoronto.ca>
2022-11-09 03:05:38 -0500

    changing abstract class to interface and creating userfactory

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,7 +1,2 @@
-package entities;
-
-public class User extends AbstractUser{
-    public User(String username, String[] userFridge) {
-        super(username, userFridge);
-    }
+package entities;public interface User {
 }
