d4f4df6a5b2da1fe57cf775f52427578de7f1a43
dangphuong <samira.dang@mail.utoronto.ca>
2022-11-14 17:26:22 -0500

    Update attribute names and styling

--- src/main/java/entities/Recipe2.java
+++ src/main/java/entities/Recipe2.java
@@ -1,10 +1,10 @@
 package entities;
-import entities.ingredient;
+import entities.Ingredient;
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
-public class recipe {
+public class Recipe {
     /**
      * recipe object to be instantiated by recipeBuilder class.
      * Information regarding a recipe is then collected in a dictionary within the instance
@@ -14,7 +14,7 @@ public class recipe {
      * Single class constructor requires name of recipe, an ArrayList of required ingredients, time to prepare
      * cost of preparation, and other info such as instructions/description.
      */
-     public recipe(String name, ArrayList<ingredient> ing, int time, int price, String info){
+     public recipe(String name, ArrayList<Ingredient> ing, int time, int price, String info){
         this.dict.put("Name", name);
         this.dict.put("IngredientList",ing);
         this.dict.put("TimeToPrepare",time);
