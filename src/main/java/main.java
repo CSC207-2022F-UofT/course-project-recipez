abc3a5aa4908b839d18737013f1f8c3d28306676
Eric Karpovits <erickarpovits@gmail.com>
2022-11-14 21:58:51 -0500

    [EK] Cleaned up the main file.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,30 +1,5 @@
-import database.Database;
-import database.DatabaseGateway;
-import fridge.Fridge;
-import fridge.Ingredient;
-
-import java.io.IOException;
-import java.util.Arrays;
-
 public class main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) {
 
-        Fridge f = new Fridge();
-        Ingredient t = new Ingredient();
-        t.setName("Tomato");
-
-        Ingredient l = new Ingredient();
-        l.setName("Lettuce");
-
-        f.addIngredient(l);
-        f.addIngredient(t);
-
-        Database database = new Database("Fridge");
-        database.store("Eric", f);
-
-
-        Database loadedDatabase = new Database("Fridge");
-        Fridge loadedFridge = (Fridge) loadedDatabase.get("Eric");
-        System.out.println("Welcome Back " +  database.getUsername() + " " + Arrays.toString(loadedFridge.allIngredientValues()));
     }
 }
