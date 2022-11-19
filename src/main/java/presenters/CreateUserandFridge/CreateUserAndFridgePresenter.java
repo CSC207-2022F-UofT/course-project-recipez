2fc09b04b77db4c80d4032d856143558c49b16f3
Shaffaan Aamir <shaffaanbin.aamir@mail.utoronto.ca>
2022-11-18 19:39:46 -0500

    trying to merge

--- src/main/java/presenters/CreateUserandFridge/CreateUserAndFridgePresenter.java
+++ src/main/java/presenters/CreateUserandFridge/CreateUserAndFridgePresenter.java
@@ -3,7 +3,6 @@ package presenters.CreateUserandFridge;
 import useCases.CreateUserAndFridge.CreateUserandFridgeResponseModel;
 
 public interface CreateUserAndFridgePresenter {
-//        CommonUser prepareSuccessView(CommonUser user);
         CreateUserandFridgeResponseModel prepareSuccessView(CreateUserandFridgeResponseModel responseModel);
 
         CreateUserandFridgeResponseModel prepareFailView(String error);
