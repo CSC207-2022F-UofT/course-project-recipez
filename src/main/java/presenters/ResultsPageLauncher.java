a8f087de8f458e8bf77595333eeffd6c40118a5f
SeongminChang <seongmin.chang@mail.utoronto.ca>
2022-11-18 20:17:27 -0500

    Created basic results page with examples of clickable recipes. ResultsPageLauncher to testrun the Frame.

---
+++ src/main/java/presenters/ResultsPageLauncher.java
@@ -0,0 +1,19 @@
+package presenters;
+
+
+import javax.swing.*;
+import java.net.MalformedURLException;
+
+public class ResultsPageLauncher {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    new ResultsPageFrame() ;
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+}
