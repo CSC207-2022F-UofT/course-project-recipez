381b1516f45da9e0cc8a4cdf411aef749ec0f17b
SeongminChang <seongmin.chang@mail.utoronto.ca>
2022-11-21 23:59:06 -0500

    deleted back button

--- src/main/java/UI/resultPage/ResultsPageView.java
+++ src/main/java/UI/resultPage/ResultsPageView.java
@@ -1,8 +1,7 @@
 package UI.resultPage;
 
-import UI.searchPageHelpFrame;
+
 import controllers.RecipeBuilderController;
-import controllers.UserEnterIngredientsController;
 import entities.recipe.Recipe;
 
 
@@ -10,8 +9,6 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.util.Dictionary;
 
@@ -24,24 +21,16 @@ public class ResultsPageView extends JFrame {
 
     ResultsPageViewModel viewModel;
 
-    UserEnterIngredientsController ingredientsController;
-
-    JTextField username;
-
     /**
      *
      * @param recipeController controller for results page, i.e recipebuilder
      * @param viewModel Data Structure used for results page
-     * @param ingredientsController controller for search page (for going back to search page button)
-     * @param username name of the user.
      */
-    ResultsPageView(RecipeBuilderController recipeController, ResultsPageViewModel viewModel,
-                    UserEnterIngredientsController ingredientsController, JTextField username)
+    ResultsPageView(RecipeBuilderController recipeController, ResultsPageViewModel viewModel)
             throws MalformedURLException {
         this.recipeController = recipeController;
         this.viewModel = viewModel;
-        this.ingredientsController = ingredientsController;
-        this.username = username;
+
 
         //Creates a result page
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -58,6 +47,7 @@ public class ResultsPageView extends JFrame {
             JLabel errorLabel = new JLabel(viewModel.errorMessage);
             panel.add(errorLabel);
         }
+
         // Adds each of the recipes as buttons if no error
         else {
             for (Recipe count : viewModel.recipes) {
@@ -73,14 +63,6 @@ public class ResultsPageView extends JFrame {
         Border resultsBorder = BorderFactory.createTitledBorder("Showing Results");
         panel.setBorder(resultsBorder);
 
-        //Back button to direct user back to search page
-        JButton backBtn = new JButton("Search Again");
-        backBtn.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                new searchPageHelpFrame(ingredientsController, username.getText());
-            }
-        });
-        panel.add(backBtn, FlowLayout.LEFT);
 
         this.setVisible(true);
         this.add(panel);
