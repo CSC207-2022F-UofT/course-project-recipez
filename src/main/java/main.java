4cfd9ba9ded61f8e962ff9ad6b03ed3265873844
Shaffaan Aamir <shaffaanbin.aamir@mail.utoronto.ca>
2022-12-02 17:29:50 -0500

    merge conflicts

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -32,32 +32,6 @@ import java.awt.*;
 
 public class main {
     public static void main(String[] args) {
-        /*
-         * create username first
-         * */
-//        Fridge userFridge = new CommonFridge();
-//        Fridge fridgeOne = new Fridge();
-//        Ingredient t = new Ingredient();
-//        t.setName("Tomato");
-//
-//        Ingredient l = new Ingredient();
-//        l.setName("Lettuce");
-//
-//        fridgeOne.addIngredient(l);
-//        fridgeOne.addIngredient(t);
-//
-//        DatabaseGateway database = new Database("Test");
-//
-//        ArrayList<Object> list = new ArrayList<>();
-//
-//        list.add(fridgeOne);
-//        String username = "Test";
-//        database.store(username, list);
-//
-//        ArrayList<Object> test = database.get(username);
-        /*
-         * Create user factories first
-         * */
         CommonFridgeFactory fridgeFactory = new CommonFridgeFactory();
         CommonUserFactory userFactory = new CommonUserFactory();
         CommonRecipeFactory recipeFactory = new CommonRecipeFactory();
@@ -85,9 +59,6 @@ public class main {
 
         CommonIngredientFactory ingredientFactory = new CommonIngredientFactory();
         UserEnterIngredientPresenter presenter1 = new UserEnterIngredientFormatter();
-/**
- * Don't know how to pass current user and current fridge
- */
         UserEnterIngredientsInputBoundary interactorforenteringingredients =
                 new UserEnterIngredientsInteractor(ingredientFactory, presenter1, databaseGateway);
 
@@ -105,31 +76,6 @@ public class main {
         screens.add(startScreen, "welcome");
         cardLayout.show(screens, "register");
         application.setVisible(true);
-
-
-
-//        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-//        System.out.println("Enter Username");
-//        String userName = myObj.nextLine();
-
-//        CommonUser shaffaan = createUserandFridgeController.create(userName);
-//        System.out.println("the user is " +
-//                presenter.prepareSuccessView(shaffaan).getName() + " the current" +
-//                        " fridge is ");
-//        System.out.println((presenter.prepareSuccessView(shaffaan).getFridge().printIngrediant()));
-
-//        Scanner myObj5 = new Scanner(System.in);
-//        System.out.println("Enter Ingrediant");
-//        String ingrediant_at_index1 = myObj5.nextLine();
-
-
-//        User_Enter_Ingrediants_Input_Boundry interactor2 = new
-//                UserEnterIngredientsInteractor(shaffaan, shaffaan.getFridge(),ingredientFactory,presenter1);
-//        User_Enter_Ingrediants_Controller userEnterIngrediantsController = new User_Enter_Ingrediants_Controller(interactor2);
-
-//        CommonIngredient ing = userEnterIngrediantsController.create(ingrediant_at_index1);
-//        System.out.println(" You have added" + presenter1.prepareSuccessView(ing).getName() + "new fridge is " +
-//                (presenter.prepareSuccessView(shaffaan).getFridge().printIngrediant()));
 
     }
 }
